dc884fc12901ef3b056d9c77e550fc5d49c51aef
KITAITI Makoto <KitaitiMakoto@gmail.com>
2016-06-10 22:35:07 +0900

    Move test class to flat space

--- test/test_rouge-lexers-fluentd.rb
+++ test/test_rouge-lexers-fluentd.rb
@@ -1,7 +1,7 @@
 require 'helper'
 require 'rouge/lexers/fluentd'
 
-class TestRouge::Lexers::Fluentd < Test::Unit::TestCase
+class TestRougeLexersFluentd < Test::Unit::TestCase
 
   def test_version
     version = Rouge::Lexers::Fluentd.const_get('VERSION')
